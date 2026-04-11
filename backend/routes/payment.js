bca622a154099656b171b2d4cb4c41a108083012
Sharath <ksharath2003@gmail.com>
2026-04-11 13:23:14 +0530

    force disable payment

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -4,10 +4,10 @@ const Razorpay = require('razorpay');
 const crypto = require('crypto');
 
 // Initialize Razorpay
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET
-});
+// const razorpay = new Razorpay({
+//   key_id: process.env.RAZORPAY_KEY_ID,
+//   key_secret: process.env.RAZORPAY_KEY_SECRET
+// });
 
 // POST create order
 router.post('/create-order', async (req, res) => {
