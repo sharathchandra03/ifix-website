2b029e406f082b4cc9fb28e5924afcbed108ce6b
Sharath <ksharath2003@gmail.com>
2026-05-14 17:55:00 +0530

    Bulk updates

--- components.js
+++ components.js
@@ -105,7 +105,7 @@
               </li>
               <li class="footer__contact-item">
                 <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z"/><circle cx="12" cy="10" r="3"/></svg>
-                Delhi · Patna · Bangalore
+                Bangalore
               </li>
             </ul>
           </div>
